ba079ce1491133a982ea89dcff8e40de30a960bc
Clarissa Milligan <milligan.s.clarissa@gmail.com>
2026-08-31 15:26:55 -0400

    math practice, basic and Math class

--- 1-Variables-IO-Math/CurrentSemester/Intro.java
+++ 1-Variables-IO-Math/CurrentSemester/Intro.java
@@ -56,5 +56,50 @@ public class Intro
         System.out.println("hi class!!");
 
 
+        // MATH !!!! //
+        double averageAge = 0.0;
+
+        int personOneAge = 29;
+        int personTwoAge = 54;
+        int personThreeAge = 21;
+
+        int personAgeSum = personOneAge + personTwoAge + personThreeAge;
+
+        // use slash for divide, use asterisk for multiplication
+        averageAge = personAgeSum / 3;                  // integer division occurs because both the divisor and dividend are ints
+                                                        // does not matter if we store in a double because the calculation is still int division
+        averageAge = personAgeSum / 3.0;                // no int division, java recognizes 3.0 as a double
+        averageAge = personAgeSum / 3.;                 // no int division, java recognizes 3. as a double
+        averageAge = ( (double) personAgeSum )/ 3;      // no int division, personAgeSum is cast to double
+        averageAge = personAgeSum / (double) 3;         // no int division, personAgeSum is cast to double
+        System.out.println(averageAge);
+
+        // type casting: change the data type of a variable
+        // put the new data type in parentheses
+        int intAverageAge = (int) averageAge;           // lossy type cast: lose everything after decimal
+        //int intAverageAge = averageAge;               // lossy conversions cannot be done without type casting in Java
+        System.out.println(intAverageAge);
+
+        //boolean booleanAverageAge = (boolean) averageAge; // can't cast things don't make sense (i.e. number into a boolean)
+
+        // Java uses ASCII characters for char type
+        // this means every character has an associated integer value
+        char charAverageAge = (char) intAverageAge;
+        // in this case, 34 is "
+        System.out.println(charAverageAge);
+
+
+        // i want to use my averageAge and calculate the area of a circle, where averageAge is the radius
+        // Math class has many other useful math constants and functions
+        // Math class exists in the lang package so it does not need imported
+        double arbitraryArea = Math.PI * Math.pow(averageAge, 2);
+        System.out.println(arbitraryArea);
+
+        // modulus operator: finds the remainder of a division operation
+        int remainder = 25 % 5;         // 25 is divisible by 5 so the remainder is 0
+        System.out.println(remainder);
+        remainder = 25 % 2;             // 25 is NOT divisible by 2 and the remainder is 1 (meaning it is odd)
+        System.out.println(remainder);
+
     }
 }
