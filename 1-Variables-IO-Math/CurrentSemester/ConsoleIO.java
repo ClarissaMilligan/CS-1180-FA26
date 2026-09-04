61532064a2ab90f8a935abd06a1e7b005cd59e9d
Clarissa Milligan <milligan.s.clarissa@gmail.com>
2026-09-04 15:25:22 -0400

    more user input practice

--- 1-Variables-IO-Math/CurrentSemester/ConsoleIO.java
+++ 1-Variables-IO-Math/CurrentSemester/ConsoleIO.java
@@ -5,18 +5,33 @@ public class ConsoleIO
     public static void main(String[] args)
     {
         System.out.println("Hello class!!!");
-        System.out.println("What is your name?");
 
+        int myFavoriteNumber = 8;
+
+        // Scanner on the RHS of the assignment is a call to the constructor
+        // Constructor is a special method in a class with the same name as the class
+        // We pass System.in to the constructor to indicate we would like to read from the console
         Scanner scnr = new Scanner(System.in);
+
+
+        System.out.println("What is your first name?");
 
         // to get a String from the user (multiple characters), I can call
         // - next() will get all the characters UNTIL there is a whitespace
         // - nextLine() will get all the characters UNTIL a newline (enter key press)
-        String userName = scnr.next();
-        userName = scnr.next();
+        String firstName = scnr.next();
 
-        System.out.println(userName);
+        System.out.println("What is your last name?");
+        String lastName = scnr.next();
 
+        System.out.println("How old are you?");
+        int userAge = scnr.nextInt();
+        int birthYear = 2026 - userAge;
 
+        System.out.println("Name: " + firstName + " " + lastName);
+        System.out.println("Birth date: " + birthYear);
+
+        char firstLetter = firstName.charAt(0);
+        System.out.println("The first letter of your name is: " + firstLetter);
     }
 }
